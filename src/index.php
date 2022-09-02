a637d8437e654d3e5903ad97c6f0c6091fd0310a
hamupapa <kmine2020@vivaldi.net>
2022-09-02 13:19:07 +0900

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My Site</title>
+  <link rel="sytlesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+lc" rel="stylesheet">
+</head>
+<body>
+
+
+<?php
+  echo "<h1>Hello</h1>";
+?>
+
+</body>
+</html>
