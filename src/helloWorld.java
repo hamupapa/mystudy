be1863cb6f01579fd9aa567d6951e0b0f72fd9d8
hamupapa <kmine2020@vivaldi.net>
2022-09-02 10:51:30 +0900

    Update and rename helloWorld.java to src/helloWorld.java

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,12 +3,15 @@ public class helloWorld {
 
 		String str = "";
 
+		// 引数が与えられていなければ「Hello, World」を表示する
 		if(args.length == 0) {
 			str = "Hello, World.";
 		}
+		// 引数が与えられていれば「Hello, <与えられた引数>」を表示する
 		else {
 			str = "Hello, " + args[0] + ".";
 		}
+		// 出力
 		System.out.println(str);
 	}
 }
